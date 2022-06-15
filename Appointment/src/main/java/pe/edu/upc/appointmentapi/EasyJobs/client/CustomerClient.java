e9a6539963110d4a179babb8aaa86d904c833855
ShoLee01 <miguelalfaro2017@gmail.com>
2022-06-15 15:51:57 -0500

    chore: Adding Hystrix update

--- Appointment/src/main/java/pe/edu/upc/appointmentapi/EasyJobs/client/CustomerClient.java
+++ Appointment/src/main/java/pe/edu/upc/appointmentapi/EasyJobs/client/CustomerClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pe.edu.upc.appointmentapi.EasyJobs.entity.Customer;
 
-@FeignClient(contextId = "Customer",name = "Accounts", path = "/customers", fallbackFactory = CustomerHystrixFallbackFactory.class)
+@FeignClient(contextId = "Customer",name = "Accounts", path = "/customers", fallback = CustomerHystrixFallbackFactory.class)
 public interface CustomerClient {
     @GetMapping(value = "/getCustomerResponse/{id}")
     public ResponseEntity<Customer> getCustomerResponse(@PathVariable("id") Long id);
