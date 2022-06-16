de3d62b631db8fd97d33bdb9a1adc5c1e1604cd5
ShoLee01 <miguelalfaro2017@gmail.com>
2022-06-16 11:56:35 -0500

    chore: Admin service

---
+++ admin-services/src/test/java/pe/edu/upc/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
